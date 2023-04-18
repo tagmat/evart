723eb2c3507e1e7f2af8c4c14834c645000c710d
Emir Tağmat <emir@tagmat.net>
2023-04-18 23:45:10 +0300

    Added simple models for designing Event Driven Architecture

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from events.models import *
 
-# Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ["name", "type", "domain", "payload", "response_payload"]
+    list_filter = ["domain"]
+
+
+class EventInline(admin.TabularInline):
+    model = Event
+
+
+class FieldInline(admin.TabularInline):
+    model = Field
+
+
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    inlines = [EventInline, ]
+
+
+class PayloadAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    inlines = [FieldInline, ]
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Domain, DomainAdmin)
+admin.site.register(Payload, PayloadAdmin)
+admin.site.register(Field)
+admin.site.register(FieldType)
+admin.site.register(Service)
+admin.site.register(Project)
+admin.site.register(EventType)
--- events/models.py
+++ events/models.py
@@ -1,3 +1,76 @@
 from django.db import models
 
-# Create your models here.
+
+class EventType(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    domain = models.ForeignKey("Domain", on_delete=models.CASCADE)
+    payload = models.ForeignKey("Payload", null=True, blank=True, on_delete=models.SET_NULL)
+    response_payload = models.ForeignKey("Payload", null=True, blank=True, on_delete=models.SET_NULL,
+                                         related_name="response_payload")
+    type = models.ForeignKey("EventType", default=1, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Domain(models.Model):
+    project = models.ForeignKey("Project", on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Payload(models.Model):
+    project = models.ForeignKey("Project", on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Field(models.Model):
+    name = models.CharField(max_length=200)
+    type = models.ForeignKey("FieldType", on_delete=models.RESTRICT)
+    payload = models.ForeignKey("Payload", on_delete=models.CASCADE)
+    required = models.BooleanField(default=False)
+    minimum = models.IntegerField(blank=True, null=True)
+    maximum = models.IntegerField(blank=True, null=True)
+    description = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class FieldType(models.Model):
+    project = models.ForeignKey("Project", on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    custom_type = models.BooleanField(default=False)
+    enum_choices = models.CharField(max_length=1000, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Service(models.Model):
+    project = models.ForeignKey("Project", on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    consumes = models.ManyToManyField("Event", related_name="event_consumers")
+    publishes = models.ManyToManyField("Event", related_name="event_publishers")
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
