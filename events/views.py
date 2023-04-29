445ce5265bc6c189cd5330ee213bc24c60e10cbb
Emir Tağmat <emir@tagmat.net>
2023-04-29 13:30:50 +0300

    Added pascal case operation id and event name

--- events/views.py
+++ events/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import HttpResponse
-from django.contrib.admin.views.decorators import staff_member_required
-from events.models import *
-from django.db.models import Q
 import yaml
+from django.contrib.admin.views.decorators import staff_member_required
+from django.db.models import Q
+from django.shortcuts import HttpResponse
+
+from events.models import *
 
 
 @staff_member_required
@@ -23,7 +24,7 @@ def generate_full_yaml(request, service_id):
                     'protocol': "kafka-secure",
                     "description": "Test broker",
                     "security": []
-                        # {"saslScram": []}
+                    # {"saslScram": []}
                 }
         },
         'defaultContentType': 'application/json',
@@ -40,22 +41,23 @@ def generate_full_yaml(request, service_id):
             'publish' if event in service.consumes.all() else 'subscribe':
                 {
                     'summary': "{0}".format(event.name),
-                    'operationId': "{1}{0}".format(event.pascal_name(), 'publish' if event in service.consumes.all() else 'subscribe'),
+                    'operationId': "{1}{0}".format(event.pascal_name(),
+                                                   'publish' if event in service.consumes.all() else 'subscribe'),
                     # 'traits': "", #$ref: '#/components/operationTraits/kafka'
                     'message': {
-                        '$ref': '#/components/messages/{0}'.format(event.name)
+                        '$ref': '#/components/messages/{0}'.format(event.pascal_name)
                     }
                 }
         }
 
-        configuration['components']['messages'][event.name] = {
+        configuration['components']['messages'][event.pascal_name] = {
             'name': "{0}".format(event.name),
             'title': "{0}".format(event.name),
             'summary': "Summary for {0} event".format(event.name),
             # 'traits':{},
         }
         if event.payload is not None:
-            configuration['components']['messages'][event.name]['payload'] = {
+            configuration['components']['messages'][camel_to_pascal_case(event.name)]['payload'] = {
                 '$ref': '#/components/schemas/{0}'.format(event.payload.name)
             }
 
