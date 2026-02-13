68e42a8c70819232ec17994cd0299ed87c5bb1b6
yuksel.ekrem <yuksel.ekrem@electrinn.com>
2026-02-13 12:34:17 +0300

    feat: Improve schema reference resolution in generate_full_yaml to handle custom object types and ensure accurate YAML exports

--- events/views.py
+++ events/views.py
@@ -507,6 +507,11 @@ def generate_full_yaml(request, service_id):
         for field in payload.field_set.all():
             field_property = {}
             x_type_override = getattr(field.type, 'x_type', None)
+            resolved_schema_ref = field.schema_ref
+            if not resolved_schema_ref and field.type.custom_type and field.type.type == 'object':
+                schema_def = field.type.get_schema_definition()
+                if schema_def:
+                    resolved_schema_ref = "#/components/schemas/{0}".format(field.type.name)
             
             if field.type.custom_type:
                 if field.type.enum_choices is not None:
@@ -544,8 +549,8 @@ def generate_full_yaml(request, service_id):
                         field_property['items'] = {'$ref': field.array_items_ref}
                 
                 # Handle schema reference
-                if field.schema_ref:
-                    field_property = {'$ref': field.schema_ref}
+                if resolved_schema_ref:
+                    field_property = {'$ref': resolved_schema_ref}
             
             # Add description if field has one (but not if field_property is a $ref)
             if field.description and '$ref' not in field_property:
